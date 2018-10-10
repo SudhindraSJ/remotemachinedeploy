adef5d0e457aebf1103efc716ef72626560c3520
AkshataSJ <sudhindr.j@gmail.com>
2018-10-10 17:51:34 +0530

    Environment changed

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ $ENVIRONMENT = "QAA" ];then
+if [ $ENVIRONMENTV = "QAA" ];then
 sshpass -p "tacouser" scp target/tacobell.war tacouser@172.17.0.4:/home/tacouser/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QA server"
          sleep 3
 sshpass -p "tacouser" ssh tacouser@172.17.0.4 "JAVA_HOME=/home/tacouser/jdk1.8.0_181"/home/tacouser/distros/apache-tomcat/bin/startup.sh
 
-elif [$ENVIRONMENT= "SITT"] 
+elif [$ENVIRONMENTV = "SITT"] 
 
 then
 
