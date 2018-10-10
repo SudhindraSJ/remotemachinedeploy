908a6370c3b9a362102d70840ca5fe5ffa8dd3eb
AkshataSJ <sudhindr.j@gmail.com>
2018-10-10 18:15:26 +0530

    Environment d

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $ENVIRONMENTV = "QAA" ];then
-sshpass -p "tacouser" scp target/tacobell.war tacouser@172.17.0.4:/home/tacouser/distros/apache-tomcat/webapps
+sshpass -p "tacouser" scp target/paytmemart.war tacouser@172.17.0.4:/home/tacouser/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QA server"
          sleep 3
 sshpass -p "tacouser" ssh tacouser@172.17.0.4 "JAVA_HOME=/home/tacouser/jdk1.8.0_181"/home/tacouser/distros/apache-tomcat/bin/startup.sh
@@ -11,7 +11,7 @@ elif [$ENVIRONMENTV = "SITT"]
 then
 
 
-sshpass -p "paytmuserp" scp target/tacobell.war paytmuserp@172.17.0.5:/home/paytmuserp/distros/apache-tomcat/webapps
+sshpass -p "paytmuserp" scp target/paytmemart.war paytmuserp@172.17.0.5:/home/paytmuserp/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QA server"
          sleep 3
 sshpass -p "paytmuserp" ssh paytmuserp@172.17.0.5 "JAVA_HOME=/home/paytmuserp/distros/jdk1.8"/home/paytmuserp/distros/apache-tomcat/bin/startup.sh
