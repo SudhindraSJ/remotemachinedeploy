10dc9635dedcaa0ed7559ba0a36a744636d68e8c
AkshataSJ <sudhindr.j@gmail.com>
2018-10-10 19:02:31 +0530

    semicolon added

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-if [ $ENVIRONMENTV = "QAA" ] then
+if [ $ENVIRONMENTV = "QAA" ]; then
 sshpass -p "tacouser" scp target/paytmemart.war tacouser@172.17.0.4:/home/tacouser/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QAA server"
          sleep 3
 sshpass -p "tacouser" ssh tacouser@172.17.0.4 "JAVA_HOME=/home/tacouser/jdk1.8.0_181"/home/tacouser/distros/apache-tomcat/bin/startup.sh
 
-elif [$ENVIRONMENTV= "SITT"] 
-
-then
+elif [ $ENVIRONMENTV= "SITT" ] then
 
 
 sshpass -p "paytmuserp" scp target/paytmemart.war paytmuserp@172.17.0.5:/home/paytmuserp/distros/apache-tomcat/webapps
