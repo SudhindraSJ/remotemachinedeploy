0725183698c5659f44a8d1d493e040e23e576024
AkshataSJ <sudhindr.j@gmail.com>
2018-10-11 12:55:09 +0530

    Multiple machine deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+
 if [ $ENVIRONMENTV = "QAA" ]; then
-sshpass -p "tacouser" scp target/paytmemart.war tacouser@172.17.0.4:/home/tacouser/distros/apache-tomcat/webapps
+
+for i in 'cat IPs.txt' 
+
+ do
+
+sshpass -p "tacouser" scp target/paytmemart.war tacouser@$i:/home/tacouser/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QAA server"
          sleep 3
-sshpass -p "tacouser" ssh tacouser@172.17.0.4 "JAVA_HOME=/home/tacouser/jdk1.8.0_181"/home/tacouser/distros/apache-tomcat/bin/startup.sh
+sshpass -p "tacouser" ssh tacouser@$i "JAVA_HOME=/home/tacouser/jdk1.8.0_181"/home/tacouser/distros/apache-tomcat/bin/startup.sh
+
+done
 
 elif [ $ENVIRONMENTV = "SITT" ]; then
 
+for i in 'cat IPs1.txt' 
 
-sshpass -p "paytmuserp" scp target/paytmemart.war paytmuserp@172.17.0.5:/home/paytmuserp/distros/apache-tomcat/webapps
+ do
+sshpass -p "paytmuserp" scp target/paytmemart.war paytmuserp@$i:/home/paytmuserp/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i SITT server"
          sleep 3
-sshpass -p "paytmuserp" ssh paytmuserp@172.17.0.5 "JAVA_HOME=/home/paytmuserp/distros/jdk1.8"/home/paytmuserp/distros/apache-tomcat/bin/startup.sh
+sshpass -p "paytmuserp" ssh paytmuserp@$i "JAVA_HOME=/home/paytmuserp/distros/jdk1.8"/home/paytmuserp/distros/apache-tomcat/bin/startup.sh
+done
+
 else
    echo "None of the condition met"
 
